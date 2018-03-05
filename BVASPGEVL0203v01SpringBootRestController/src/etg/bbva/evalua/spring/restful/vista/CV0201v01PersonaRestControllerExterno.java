ba2719a91eae202a77ea5ba8b5fdc078d502b3cc
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2018-03-05 16:38:13 -0500

    Spring Rest interno y externo en una clase

---
+++ BVASPGEVL0203v01SpringBootRestController/src/etg/bbva/evalua/spring/restful/vista/CV0201v01PersonaRestControllerExterno.java
@@ -0,0 +1,26 @@
+package etg.bbva.evalua.spring.restful.vista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/*
+ * A partir de la versión 1.2 de Spring se define:
+ * @SpringBootApplication es equivalente a utilizar @Configuration + @EnableAutoConfiguration + @ComponentScan
+ * 
+ * @Configuration: Indica que esta es una clase usada para configurar el contenedor Spring.
+ * @ComponentScan: Escanea los paquetes de nuestro proyecto en busca de los componentes que hayamos creado, ellos son,  
+ * 				   las clases que utilizan las siguientes anotaciones: @Component, @Service, @Controller, @Repository. 
+ */
+@SpringBootApplication
+@ComponentScan("etg")
+public class CV0201v01PersonaRestControllerExterno {
+
+	public static void main(String[] args) {
+		System.out.println("1. Para iniciar el servicio Rest se requieres @RestController");
+		System.out.println("2. El mapping @RequestMapping para el url está en la clase CCPersona");
+		
+		SpringApplication.run(CV0201v01PersonaRestControllerExterno.class, args);
+		System.out.println("3. Ejecutar en la url : localhost:8080/listado");		
+	}
+}
