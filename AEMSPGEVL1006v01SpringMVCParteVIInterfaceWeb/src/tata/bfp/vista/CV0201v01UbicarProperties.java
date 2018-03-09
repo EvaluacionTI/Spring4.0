62b04f50afabb24e0d255f12f717053cb1217b8a
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2018-03-09 14:32:39 -0500

    Spring MVC Parte VI Interface Web con jstl y taglib

---
+++ AEMSPGEVL1006v01SpringMVCParteVIInterfaceWeb/src/tata/bfp/vista/CV0201v01UbicarProperties.java
@@ -0,0 +1,9 @@
+package tata.bfp.vista;
+
+public class CV0201v01UbicarProperties {
+	public static void main(String[] args) {
+		System.out.println(System.getProperties());
+		System.out.println(System.getProperty("user.dir"));
+	}
+	
+}
