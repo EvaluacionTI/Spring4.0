e32e84a2a922f1c176997c5a3a7d4f8aceeb706b
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2018-04-10 11:53:06 -0500

    Spring JDBC : Conexion with AS400

---
+++ AEMSPGEVL0301v01EvaluaSpringJdbcAS400/src/tata/bfp/servicio/as400/CICliente.java
@@ -0,0 +1,14 @@
+package tata.bfp.servicio.as400;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import tata.bfp.entidad.as400.CECliente;
+
+public interface CICliente {
+	public void agregarCliente(CECliente poCECliente) throws SQLException;
+	public void actualizarCliente(CECliente poCECliente) throws SQLException;
+	public void eliminarCliente(int piIdCodigo);
+	public CECliente buscarClienteporCodigo(int piIdCodigo);	
+	public List<CECliente> listaCliente();
+}
