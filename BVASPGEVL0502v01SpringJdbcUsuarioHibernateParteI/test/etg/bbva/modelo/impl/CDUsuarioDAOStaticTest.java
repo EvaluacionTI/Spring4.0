4ea30f763b1d310f55d68132ee663d4dc6858733
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2018-04-12 11:37:19 -0500

    Spring JDBC I: Configurar bean usuario con spring y dependencias pom

---
+++ BVASPGEVL0502v01SpringJdbcUsuarioHibernateParteI/test/etg/bbva/modelo/impl/CDUsuarioDAOStaticTest.java
@@ -0,0 +1,70 @@
+package etg.bbva.modelo.impl;
+
+import static org.junit.Assert.assertNotEquals;
+
+import java.io.IOException;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import etg.bbva.entidad.CE01Usuario;
+import etg.bbva.modelo.impl.CDUsuarioDAOStatic;
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+public class CDUsuarioDAOStaticTest extends TestCase {
+	private static final Logger moLog = LogManager.getLogger(CDUsuarioDAOStaticTest.class);
+	
+	private ClassPathXmlApplicationContext oCntx;
+	private CDUsuarioDAOStatic oCDUsuario;
+	private CE01Usuario oCEUsuario;
+	
+	protected void setUp() throws IOException{
+		moLog.debug("Setup del Test");
+		// Para el test es necesario copiar el archivo applicationContext.xml en la carpeta resources
+		String[] oPaths = {"applicationContext.xml"};
+		oCntx = new ClassPathXmlApplicationContext(oPaths);
+		oCDUsuario = (CDUsuarioDAOStatic) oCntx.getBean("idCDUsuarioStatic");
+		moLog.debug("Hemos obtenido el objeto que implementa CIUsuario");
+	}
+	
+	protected void tearDown() {
+		oCEUsuario = null;
+		oCDUsuario = null;
+	}
+
+	public void testAddFindBorrar() {
+		oCEUsuario = oCDUsuario.findUsuario(new Integer (1));
+		moLog.info("=====> testAddFindBorrar");
+		moLog.info("=====> " + oCEUsuario);
+		
+		// Para verificar que hay conexion y no salta excepcion
+		oCEUsuario = new CE01Usuario();
+		oCEUsuario.setId(new Integer(15));
+		oCEUsuario.setNombre("Arquitectura");
+		oCDUsuario.saveUsuario(oCEUsuario);
+		moLog.info("=====> " + oCEUsuario);
+		
+		assertTrue(oCEUsuario!=null);
+		
+		CE01Usuario oCEUsuario2 = oCDUsuario.findUsuario(new Integer(1));
+		moLog.info("=====> [Recuperado usuario =" + oCEUsuario2 + "]");
+		assertFalse(oCEUsuario2!=null);
+		moLog.info("=====> [Comparamos = " + oCEUsuario2 + " con " + oCEUsuario + "]");
+		assertNotEquals(oCEUsuario, oCEUsuario2);
+		
+		//Recuperamos el mismo usuario
+		oCDUsuario.deleteUsuario(new Integer(1));
+		oCEUsuario2 = oCDUsuario.findUsuario(new Integer(1));
+		assertNull("El usuario no debe existir", oCEUsuario2);
+		moLog.info("=====> [Eliminado = " + oCEUsuario2 + "]");
+
+	}
+	
+	public static void main(String[] args) {
+		TestRunner.run(CDUsuarioDAOStaticTest.class);
+
+	}
+
+}
